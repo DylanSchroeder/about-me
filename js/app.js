51c3cfaf9c426a5ea3e0f9465233c9ad4796718d
Dylan Schroeder <schrodac@gmail.com>
2018-06-05 15:16:52 -0500

    questions 2-5 all are entered and can be answered with any Y or N value

--- js/app.js
+++ js/app.js
@@ -11,48 +11,54 @@ else if (sanitizedAnswer === 'N' || sanitizedAnswer === "NO"){
 else {
     alert("Not a recognizable answer");
 }
-/*
-var secondQuestion = prompt('Am I a male?');
-if (secondQuestion === 'Y'){
+
+var secondQuestion = prompt('Am I female?');
+var sanitizedAnswer = secondQuestion.toUpperCase().trim();
+if (sanitizedAnswer === 'Y' || sanitizedAnswer === "YES"){
     alert('yes!');
 }
-else if (secondQuestion === 'N'){
-    alert('no!')
+else if (sanitizedAnswer === 'N' || sanitizedAnswer === "NO"){
+    alert('no!');
 }
 else {
-    alert("Not a recognizable answer")
+    alert("Not a recognizable answer");
 }
-var thirdQuestion = prompt('Do I eat food?');
-if (thirdQuestion === 'Y'){
+
+var thirdQuestion = prompt('Do I like animals?');
+var sanitizedAnswer = thirdQuestion.toUpperCase().trim();
+if (sanitizedAnswer === 'Y' || sanitizedAnswer === "YES"){
     alert('yes!');
 }
-else if (thirdQuestion === 'N'){
-    alert('no!')
+else if (sanitizedAnswer === 'N' || sanitizedAnswer === "NO"){
+    alert('no!');
 }
 else {
-    alert("Not a recognizable answer")
+    alert("Not a recognizable answer");
 }
-var fourthQuestion = prompt('Do I go outside?');
-if (fourthQuestion === 'Y'){
+
+var fourthQuestion = prompt('Am I nervous about this class?');
+var sanitizedAnswer = fourthQuestion.toUpperCase().trim();
+if (sanitizedAnswer === 'Y' || sanitizedAnswer === "YES"){
     alert('yes!');
 }
-else if (fourthQuestion === 'N'){
-    alert('no!')
+else if (sanitizedAnswer === 'N' || sanitizedAnswer === "NO"){
+    alert('no!');
 }
 else {
-    alert("Not a recognizable answer")
+    alert("Not a recognizable answer");
 }
-var fifthQuestion = prompt('Do I own a pet?');
-if (fifthQuestion === 'Y'){
+
+var fifthQuestion = prompt('Am I glad to be in this class?');
+var sanitizedAnswer = fifthQuestion.toUpperCase().trim();
+if (sanitizedAnswer === 'Y' || sanitizedAnswer === "YES"){
     alert('yes!');
 }
-else if (fifthQuestion === 'N'){
-    alert('no!')
+else if (sanitizedAnswer === 'N' || sanitizedAnswer === "NO"){
+    alert('no!');
 }
 else {
-    alert("Not a recognizable answer") 
+    alert("Not a recognizable answer");
 }
-*/
 
 
 
