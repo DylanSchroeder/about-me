d808bcfb881b44b610b40e39a534d523e6879d00
Dylan Schroeder <schrodac@gmail.com>
2018-06-05 14:26:51 -0500

    added 5 sample questions in js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,28 @@
 'use strict';
 
-var answer = prompt('Do you think yes or no');
+var question1 = prompt('Am I from Earth?');
+var q2 = prompt('Am I a male?');
+var q3 = prompt('Do I eat food?');
+var q4 = prompt('Do I go outside?');
+var q5 = prompt('Do I own a pet?');
+
+if (question1 === 'Y'){
+    alert('yes!');
+}
+else if (question1 === 'N'){
+    alert('no!')
+}
+else {
+    alert("Not a recognizable answer")
+}
+
+if (q2 === 'Y'){
+    alert('yes!');
+}
+else if (q2 === 'N'){
+    alert('no!')
+}
+else {
+    alert("Not a recognizable answer")
+}
+
